ac538fecad17bc013ed426f79de3d24fe2933962
Sabyasachi Das <sada0419@netcracker.com>
2020-10-09 23:52:39 +0530

    Multiple markers with dynamic popup

--- map1.py
+++ map1.py
@@ -7,10 +7,22 @@ Created on Fri Oct  9 21:34:01 2020
 
 
 import folium
-map=folium.Map(location=[50.708611, -105.179628],zoom_start=3)
+import pandas as pd
+
+#Read file to a dataframe
+data = pd.read_csv("Volcanoes.txt")
+lat = list(data['LAT'])
+lon = list(data['LON'])
+name = list(data['NAME'])
+ele = list(data['ELEV'])
+
+map = folium.Map(location=[50.708611, -105.179628],zoom_start=3)
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location = [50.01001,-105.5433], popup = "Hi. I am a Marker" , icon=folium.Icon(color='green')))
+
+for lt,ln,el,nm in zip(lat,lon,ele,name):
+    fg.add_child(folium.Marker(location = [lt,ln], popup = nm + " " + str(el) + ' mts' , icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("map1.html")
