afb0409ba62322c5bbc5576eaee6010d21e48e6f
Sabyasachi Das <sada0419@netcracker.com>
2020-10-09 23:04:08 +0530

    Base map with a marker

--- map1.py
+++ map1.py
@@ -7,7 +7,7 @@ Created on Fri Oct  9 21:34:01 2020
 
 
 import folium
-map=folium.Map(location=[50.708611, -105.179628],zoom_start=3,tiles= "Stamen Terrain")
+map=folium.Map(location=[50.708611, -105.179628],zoom_start=3)
 
 fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.Marker(location = [50.01001,-105.5433], popup = "Hi. I am a Marker" , icon=folium.Icon(color='green')))
